ee933746ce9f8ad64ddca1b86a424c79449a9d86
francisconicolasferri <francisconicolasferri00@gmail.com>
2022-10-02 17:30:38 -0300

    modificado localmente el archivo nav

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav>
+<ol>
+    <li><a href="#">home</a><li>
+    <li><a href="#">Quienes somos</a><li>
+    <li><a href="#">Nosostros</a><li>
+    <li><a href="#">Contacto</a><li>
+    
+</ol>
+</nav>
